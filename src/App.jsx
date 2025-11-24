d94aec11f4490fe81e3e217ff9150b20cc3389ec
chandan <chandansamantaray784@gmail.com>
2025-11-24 20:10:51 +0530

    bootm nav added

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,17 @@ import Navbar from "./components/Navabar";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
+import BottomNav from "./components/BottomNav";
 
 export default function App() {
   return (
     <div className="bg-white text-gray-900 overflow-x-hidden">
       <div className="max-w-[1440px] mx-auto w-full">
-        
-
-      
         <Navbar />
-        <ScrollToTop/>
+        <ScrollToTop />
         <Outlet />
         <Footer />
+        <BottomNav />
       </div>
     </div>
   );
---
+++ src/components/pages/blog/BlogHomePage.jsx
@@ -0,0 +1,17 @@
+export default function BlogHome() {
+  return (
+    <div className="w-full h-[90vh] flex items-center justify-center bg-gray-50">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-gray-800 mb-4">Blog Section</h1>
+
+        <p className="text-lg text-gray-500 mb-6">
+          We're working on something amazing!
+        </p>
+
+        <span className="px-6 py-3 rounded-2xl bg-blue-600 text-white text-lg font-semibold shadow-lg">
+          🚀 Coming Soon
+        </span>
+      </div>
+    </div>
+  );
+}
